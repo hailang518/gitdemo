cd99157a41336474bbfba04658ffbbece2fb0b47
hailang518 <hailang518@163.com>
2020-05-24 22:49:11 +0800

    11

--- src/main/java/com/xy/App.java
+++ src/main/java/com/xy/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World2222!" );
+        System.out.println( "Hello World22333!" );
     }
 }
