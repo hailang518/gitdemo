7f9abe98838ebadeebbc08eccd5aeffe9644d91f
hailang518 <hailang518@163.com>
2020-05-24 22:35:23 +0800

    22

---
+++ src/main/java/com/xy/App.java
@@ -0,0 +1,14 @@
+package com.xy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World2222!" );
+    }
+}
